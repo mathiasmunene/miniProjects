ae5b93f1cb125d1b58a80df768b4581068ab0d78
Mathias Munene <munenemathias@gmail.com>
2025-01-31 05:17:38 +0300

    final commit

--- colorFlipper/scripts/scripts.js
+++ colorFlipper/scripts/scripts.js
@@ -5,10 +5,12 @@ function setColor(name) {
 }
 
 function randomColor() {
-    const red = Math.round(Math.random() ^ 255);
-    const green = Math.round(Math.random() ^ 255);
-    const blue = Math.round(Math.random() ^ 255);
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
 
     const color = `rgb(${red}, ${green}, ${blue})`;
     body.style.backgroundColor = color;
 }
+
+randomColor();
