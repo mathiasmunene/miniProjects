a3e03fe1104883b254b1b59dad63cb4b10db4c0f
Mathias Munene <munenemathias@gmail.com>
2025-01-31 07:32:16 +0300

    bug fixed

--- palindromeChecker/scripts/index.js
+++ palindromeChecker/scripts/index.js
@@ -1,1 +1,23 @@
-fff
+const input = document.getElementById("input");
+
+function check() {
+    const value = input.value
+    alert(value)
+}
+
+function reverseString (str) {
+    return str.split("").reverse().join("")
+}
+
+function check() {
+    const value = input.value;
+    const reverse = reverseString(value);
+
+    if (value === reverse) {
+        alert("PALINDROME");
+    } else {
+        alert("Not today!");
+    }
+
+    input.value = ""
+} 
