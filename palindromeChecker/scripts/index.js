71082c7a622637b6c17e3af251d825f7bf2d8c9b
Mathias Munene <munenemathias@gmail.com>
2025-01-31 07:38:06 +0300

    h1 corrected

--- palindromeChecker/scripts/index.js
+++ palindromeChecker/scripts/index.js
@@ -1,13 +1,9 @@
 const input = document.getElementById("input");
 
-function check() {
-    const value = input.value
-    alert(value)
-}
 
-function reverseString (str) {
+/*function reverseString (str) {
     return str.split("").reverse().join("")
-}
+}*/
 
 function check() {
     const value = input.value;
